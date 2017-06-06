79522723b3776f4aeda4844dbd27c833cd57bdca
Yelitza Cho <yelitza.choque@gmail.com>
2017-06-06 18:39:06 -0500

    Empezando a trabajar con funcionalidades

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@
 const render = (root) => {
   root.empty();
   const wrapper = $('<div class="wrapper"></div>');
+
   wrapper.append(Header(_ => render(root)));
   root.append(wrapper);
 }
@@ -24,4 +25,23 @@ $( _ => {
     render(root);
   });
 
+  $(input).on('keyup',(e) => {
+    
+  });
+
 });
+
+// const showMap = () => {
+//   const row = $('<div class="row">');
+//   const containerMap = $('<div class="containerMap"></div>');
+//   const name = $('<h4 class="col s9">'+ state.setectedas+'</h4>');
+//   const address = $('<p class="col s9">'+ state.selectedStations +'</p>');
+//   const district = $('<p class="col s9"><span>Hola</span></p>');
+//   const containerInfo = $('<div class="container">');
+
+  // containerInfo.append(name);
+  // containerInfo.append(address);
+  // containerInfo.append(district);
+  // containerMap.append(containerInfo);
+  // row.append(containerMap);
+// }
--- js/utils/filters.js
+++ js/utils/filters.js
@@ -1,5 +1,7 @@
 'use strict';
 
 const filterByDistrict = (stations,query) => {
+	const filter = state..map(e => e.district).indexOf(query);
 
+	console.log(filter);
 }
